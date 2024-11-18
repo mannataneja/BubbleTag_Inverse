6ae6770bede4eb422401f35f3acd9f68c6f9188f
mannataneja <tt3092@g.rit.edu>
2024-11-18 14:27:36 -0500

    TimeBetweenSpawns progression added

--- Assets/Scripts/Bubbles.cs
+++ Assets/Scripts/Bubbles.cs
@@ -16,7 +16,7 @@ public class Bubbles : MonoBehaviour
     public ParticleSystem pop;
     
 
-    float maxHeight = 5f;
+    float maxHeight = 7f;
 
     GameObject animal;
     Rigidbody animalRB;
