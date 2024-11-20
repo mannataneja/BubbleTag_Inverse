9ebeb11c3499ea34f654ee68e24156156a880bf8
AyushiParikh-RIT <ap4082@rit.edu>
2024-11-20 14:38:55 -0500

    Adding name inputfield

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using static UnityEngine.Rendering.DebugUI;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private CustomButton startBtn;
+    [SerializeField] private TMP_InputField nameInput;
 
     public void StartGame_Btn()
     {
@@ -15,14 +18,18 @@ public class MenuManager : MonoBehaviour
 
     public void Update()
     {
-       if(startBtn.IsButtonHighlighted())
-       {
-            Debug.Log("in");
-            if (OVRInput.Get(OVRInput.Button.One))
+        if (!string.IsNullOrEmpty(nameInput.text))
+        {
+            startBtn.interactable = true;
+            if (startBtn.IsButtonHighlighted())
             {
-                Debug.Log("true");
-                StartGame_Btn();
+                Debug.Log("in");
+                if (OVRInput.Get(OVRInput.Button.One))
+                {
+                    Debug.Log("true");
+                    StartGame_Btn();
+                }
             }
-       }     
+        }
     }
 }
