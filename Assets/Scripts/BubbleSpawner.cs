ca7364bd3c53fe6519beefa1ca2f2bf776764e37
AyushiParikh-RIT <ap4082@rit.edu>
2025-01-16 09:58:27 -0500

    Adding distractor bubbles

--- Assets/Scripts/BubbleSpawner.cs
+++ Assets/Scripts/BubbleSpawner.cs
@@ -6,13 +6,30 @@ using UnityEngine;
 public class BubbleSpawner : MonoBehaviour
 {
     public Bubbles bubble;
+    public bool distractorBubble = false;
+    public Transform[] spawnDistractorLocations = new Transform[4];
+
 
     public void SpawnBubble()
     {
-        Instantiate(bubble, transform);
+        if (distractorBubble)
+        {
+            bubble.SetFlickerMaterial();
+            Transform transform = SpawnDistractorBubble();
+            Instantiate(bubble, transform);
+        }
+        else
+        {
+            Instantiate(bubble, transform);
+        }
         Vector3 temp = bubble.transform.position;
         temp.x += Random.Range(-0.3f, 0.3f);
         bubble.transform.position = temp;
         temp = new Vector3(0, 0 , 0);   
+    }
+
+    public Transform SpawnDistractorBubble()
+    {
+        return spawnDistractorLocations[Random.Range(0, spawnDistractorLocations.Length)];
     }
 }
