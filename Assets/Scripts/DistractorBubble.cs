541942d30e59ffdc9c478acc9baceee32430908b
AyushiParikh-RIT <ap4082@rit.edu>
2025-01-17 15:14:00 -0500

    Adding a check in the distractor bubble
    Check if the distractor bubble animal is never the current animal

--- Assets/Scripts/DistractorBubble.cs
+++ Assets/Scripts/DistractorBubble.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class DistractorBubble : MonoBehaviour
 {
     public GameObject[] animals;
+
     public float distractorSpeed = 5f;
     float distractorMaxHeight = 3f;
+    private GameManager gameManager;
+    private int animalIndex;
     GameObject animal;
 
     private void Start()
     {
-       // Instantiate(animals[Random.Range(0, (animals.Length - 1))], transform);
+        gameManager = GameObject.FindAnyObjectByType<GameManager>();
         animal = transform.GetChild(transform.childCount - 1).gameObject;
         animal.GetComponent<Rigidbody>().useGravity = true;
+    }
+    private void Awake()
+    {
+        InstantiateAnimal();
     }
 
     void Update()
     {
         transform.Translate(Vector3.up * distractorSpeed * Time.deltaTime);
-         
-        if (transform.position.y >= distractorMaxHeight) 
+
+        if (transform.position.y >= distractorMaxHeight)
         {
             GameObject.FindAnyObjectByType<DistractorBubbleSpawner>().spawnDistractorBubble();
             Destroy(gameObject);
         }
+    }
+
+    void InstantiateAnimal()
+    {
+        animalIndex = Random.Range(0, (animals.Length - 1));
+        while (gameManager.currentAnimalIndex == animalIndex)
+        {
+            animalIndex = Random.Range(0, (animals.Length - 1));
+        }
+        Instantiate(animals[animalIndex], transform);
     }
 }
