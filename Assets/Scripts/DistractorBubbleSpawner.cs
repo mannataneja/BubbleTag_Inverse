386f0ba42f54a8489e733098879da93604dcfc4d
mannataneja <tt3092@g.rit.edu>
2025-01-23 13:37:59 -0500

    Distractor bubble option for spawn interval and number of spawners added

--- Assets/Scripts/DistractorBubbleSpawner.cs
+++ Assets/Scripts/DistractorBubbleSpawner.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class DistractorBubbleSpawner : MonoBehaviour
 {
     public GameObject distractorBubble;
+    public float spawnInterval;
+    public int numberOfDistractorSpawners;
     public Transform[] spawnDistractorLocations = new Transform[4];
 
     public Material flickerMaterial;
-
-    public GameManager gameManager;
 
     int distractorAnimalIndex;
     private void Start()
@@ -23,31 +23,16 @@ public class DistractorBubbleSpawner : MonoBehaviour
         {
             Debug.Log("sntg");
 
-            GameObject newDistacrtorBubble = Instantiate(distractorBubble, spawnDistractorLocations[Random.Range(0, (spawnDistractorLocations.Length - 1))]);
+            GameObject newDistacrtorBubble = Instantiate(distractorBubble, spawnDistractorLocations[Random.Range(0, numberOfDistractorSpawners)]);
             newDistacrtorBubble.GetComponent<Bubbles>().SetFlickerMaterial();
             newDistacrtorBubble.GetComponent<Bubbles>().isDistractor = true;
-
-
-/*            distractorAnimalIndex = Random.Range(0, gameManager.animalIndex.Length + 1);
-            if(distractorAnimalIndex == gameManager.currentAnimalIndex)
-            {
-                if(distractorAnimalIndex < gameManager.animalIndex.Length - 1)
-                {
-                    distractorAnimalIndex++;
-                }
-                else
-                {
-                    distractorAnimalIndex--;
-                }
-            }
-            newDistacrtorBubble.GetComponent<Bubbles>().animalIndex = distractorAnimalIndex;*/
 
             Vector3 temp = newDistacrtorBubble.transform.position;
             temp.x += Random.Range(-0.3f, 0.3f);
             newDistacrtorBubble.transform.position = temp;
             temp = new Vector3(0, 0, 0);
 
-            yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(spawnInterval);
         }
 
     }
